9fef4d7433b192ec8e69a2aa835a911c662e8cbd
James O'Gorman <james@netinertia.co.uk>
2023-02-12 14:53:38 +0000

    ByteBuffer: Extract boilerplate to indexOf method
    readBytes and readString were basically the same, but returning a
    different value.
    
    Extract that duplicated code out into a method that finds the index to
    simplify the code.

--- Sources/AgeKit/Extensions/ByteBuffer.swift
+++ Sources/AgeKit/Extensions/ByteBuffer.swift
@@ -19,43 +19,39 @@ extension ByteBuffer {
         self = ByteBufferAllocator().buffer(bytes: buf)
     }
 
-    /// Read the buffer until `delim` is found, move the reader index forward by the length of the data
-    /// and return the result as `[UInt8]`, or `nil` if `delim` was not found.
-    mutating func readBytes(until delim: Character) -> [UInt8]? {
+    mutating private func indexOf(delim: Character) -> Array<UInt8>.Index? {
         var pos = self.readerIndex
         let bufSize = 4096
         let char = delim.asciiValue!
         while self.readableBytes >= pos {
             let length = (bufSize <= self.readableBytes ? bufSize : self.readableBytes)
-            guard let b = self.getBytes(at: pos, length: length) else {
+            guard let bytes = self.getBytes(at: pos, length: length) else {
                 return nil
             }
-            guard let i = b.firstIndex(of: char) else {
+            guard let index = bytes.firstIndex(of: char) else {
                 pos += bufSize
                 continue
             }
-            return self.readBytes(length: i+1)
+            return index+1
         }
         return nil
+    }
+
+    /// Read the buffer until `delim` is found, move the reader index forward by the length of the data
+    /// and return the result as `[UInt8]`, or `nil` if `delim` was not found.
+    mutating func readBytes(until delim: Character) -> [UInt8]? {
+        guard let index = self.indexOf(delim: delim) else {
+            return nil
+        }
+        return self.readBytes(length: index)
     }
 
     /// Read the buffer until `delim` is found, decoding is as String using the UTF-8 encoding.
     /// The reader index is moved forward by the length of the string found.
     mutating func readString(until delim: Character) -> String? {
-        var pos = self.readerIndex
-        let bufSize = 4096
-        let char = delim.asciiValue!
-        while self.readableBytes >= pos {
-            let length = (bufSize <= self.readableBytes ? bufSize : self.readableBytes)
-            guard let b = self.getBytes(at: pos, length: length) else {
-                return nil
-            }
-            guard let i = b.firstIndex(of: char) else {
-                pos += bufSize
-                continue
-            }
-            return self.readString(length: i+1)
+        guard let index = self.indexOf(delim: delim) else {
+            return nil
         }
-        return nil
+        return self.readString(length: index)
     }
 }
